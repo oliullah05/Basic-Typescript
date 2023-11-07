212b8932ba393a9436ecb5ec17523f9e7999ea0c
oliullah05 <mdolihasan@gmail.com>
2023-11-07 16:26:49 +0600

    Object Oriented Programming in TypeScript

---
+++ module-3/3.1.ts
@@ -0,0 +1,29 @@
+{
+    // 
+             //    Class and object
+
+class Animal {
+    name:string;
+    species:string;
+    sound:string;
+    constructor(name:string, species:string, sound:string){
+        this.name=name;
+        this.species=species;
+        this.sound=sound;
+    }
+}
+
+const createDog = new Animal("Mikel","dog","Ghew Ghew")
+
+const createCat = new Animal ("Jerry","Cat","Meaw Meaw")
+
+
+
+
+
+
+
+
+
+    // 
+}
